de52ca3d43f904bcd1f5bee2c694b4fb52c5f53e
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-27 01:56:06 +0530

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -8,27 +8,5 @@ cd postfix
 make makefiles CCARGS="${CFLAGS}"
 make
 BASE=$PWD
-
-
-rm -rf afl-build
-git clone --depth=1 https://github.com/AFLplusplus/AFLplusplus afl-build
-cd afl-build
-make source-only
-ar ru FuzzingEngine.a afl-compiler-rt.o utils/aflpp_driver/aflpp_driver.o 
-
-cp -f FuzzingEngine.a afl-fuzz afl-showmap ../
-echo "Success: link fuzz target against FuzzingEngine.a!"
-
-# Compile target using ASan, coverage instrumentation, and link against FuzzingEngine.a
-$CC -fsanitize=address .clusterfuzzlite/fuzz_mime.c FuzzingEngine.a -o fuzz_mime
-$CC -fsanitize=address .clusterfuzzlite/fuzz_tok822.c FuzzingEngine.a -o fuzz_tok822
-
-# Test out the build by fuzzing it. INPUT_CORPUS is a directory containing files. Ctrl-C when done.
-
-AFL_SKIP_CPUFREQ=1 ./afl-fuzz -i $INPUT_CORPUS1 -o output -m none ./fuzz_mime
-AFL_SKIP_CPUFREQ=1 ./afl-fuzz -i $INPUT_CORPUS2 -o output -m none ./fuzz_tok822
-
-
-# Create a fuzzer build to upload to ClusterFuzz.
-zip fuzz_mime-build.zip fuzz_mime afl-fuzz afl-showmap
-zip fuzz_tok822-build.zip fuzz_tok822 afl-fuzz afl-showmap
+./afl-cc -fsanitize=address,undefined -ggdb -00 fuzz_mime.c fuzz_tok822.c -o fuzzer
+./afl-fuzz -i in -o out -m none -- ./fuzzer @@
