2ea57175efc45169e6c1980aa279d18535d68f51
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-27 15:08:15 +0100

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -4,13 +4,17 @@ export CC CXX
 set $LLVM_CONFIG=/usr/bin/llvm-config-10
 
 
+
+
 cd postfix
 make makefiles CCARGS="${CFLAGS}"
 make
 BASE=$PWD
 
 git clone https://github.com/AFLplusplus/AFLplusplus
+cp -r .clusterfuzzlite/fuzz_mime.c /AFLplusplus
 cd AFLplusplus
 make all
 make install
-
+./afl-cc -fsanitize=address,undefined -ggdb fuzz_mime.c -o fuzzer
+./afl-fuzz -i in -o out -m none -- ./fuzzer @@
